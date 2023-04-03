86da6c16fd73aec43f744ed72829c03a53f83089
chlouzada <chlouzada@gmail.com>
2023-04-03 19:48:37 -0300

    use flask

--- api/index.py
+++ api/index.py
@@ -1,18 +1,16 @@
-from http.server import BaseHTTPRequestHandler
+from flask import Flask, request
 import tiktoken
 
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        string = "This is a test for token count"
-        encoding_name = "cl100k_base"
+app = Flask(__name__)
 
-        encoding = tiktoken.get_encoding(encoding_name)
-        num_tokens = len(encoding.encode(string))
-
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write(str(num_tokens).encode('utf-8'))
-        return
+@app.route('/')
+def index():
     
+
+    string = "This is a test for token count"
+    encoding_name = "cl100k_base"
+
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return str(num_tokens).encode('utf-8')
+
