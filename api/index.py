47ffb03671c3535f543c1a6298cf8b03603b8486
chlouzada <chlouzada@gmail.com>
2023-04-03 19:35:40 -0300

    num tokens

--- api/index.py
+++ api/index.py
@@ -1,10 +1,18 @@
 from http.server import BaseHTTPRequestHandler
- 
+import tiktoken
+
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
+        string = self.path.split('?')[1].split('=')[1]
+        encoding_name = self.path.split('?')[2].split('=')[1]
+
+        encoding = tiktoken.get_encoding(encoding_name)
+        num_tokens = len(encoding.encode(string))
+
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(str(num_tokens).encode('utf-8'))
         return
+    
